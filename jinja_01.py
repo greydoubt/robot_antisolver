668683f48b76a5d5da0698e03b31a2b764039402
greydoubt <43443470+greydoubt@users.noreply.github.com>
2025-02-07 23:03:28 -0800

    Create jinja_01.py

---
+++ jinja_01.py
@@ -0,0 +1,15 @@
+# https://github.com/pallets/jinja/blob/main/examples/basic/inheritance.py
+
+from jinja2 import Environment
+from jinja2.loaders import DictLoader
+
+env = Environment(
+    loader=DictLoader(
+        {
+            "a": "[A[{% block body %}{% endblock %}]]",
+            "b": "{% extends 'a' %}{% block body %}[B]{% endblock %}",
+            "c": "{% extends 'b' %}{% block body %}###{{ super() }}###{% endblock %}",
+        }
+    )
+)
+print(env.get_template("c").render())
